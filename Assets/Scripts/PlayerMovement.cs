508e841c1c78fcea4663b5b23c86c455eea77c72
Joshua Watson <josh.m.watson@gmail.com>
2019-10-04 20:09:15 -0500

    Added Player Movement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+
+    public Rigidbody2D Rb;
+
+    private Vector2 _movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Register Input
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //movement
+        Rb.MovePosition(Rb.position + _movement.normalized * MoveSpeed * Time.fixedDeltaTime);
+    }
+}
