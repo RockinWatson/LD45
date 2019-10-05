dfd073c2aa78140040348a3c7dd3e13d1d76178d
Joshua Watson <josh.m.watson@gmail.com>
2019-10-05 07:55:37 -0500

    Added Camera Bounds to world

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,25 @@ public class CameraFollow : MonoBehaviour
     public Transform PlayerTransform;
     public float FollowSpeed = 2f;
 
+    public bool Bounds;
+
+    public Vector3 MinCameraPos;
+    public Vector3 MaxCameraPos;
+
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 newPosition = PlayerTransform.position;
         newPosition.z = -10;
         transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+
+        //Camera Bounds
+        if (Bounds)
+        {
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x, MinCameraPos.x, MaxCameraPos.x),
+                Mathf.Clamp(transform.position.y, MinCameraPos.y, MaxCameraPos.y),
+                Mathf.Clamp(transform.position.z, MinCameraPos.z, MaxCameraPos.z));
+        }
     }
 }
