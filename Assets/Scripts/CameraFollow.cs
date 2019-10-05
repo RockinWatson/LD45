054842799cf60ceed2fe6a821e37034cd24fe5dd
Joshua Watson <josh.m.watson@gmail.com>
2019-10-04 22:10:45 -0500

    Added Follow Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public float FollowSpeed = 2f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 newPosition = PlayerTransform.position;
+        newPosition.z = -10;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+    }
+}
