4666c7727d822e6cbf96a5eff991e1b63711a05f
Joshua Watson <josh.m.watson@gmail.com>
2019-10-06 07:55:27 -0500

    Added Camera Y axis offset to see houses

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,11 +10,16 @@ public class CameraFollow : MonoBehaviour
     public Vector3 MinCameraPos;
     public Vector3 MaxCameraPos;
 
+    private void Start()
+    {
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 newPosition = PlayerTransform.position;
         newPosition.z = -10;
+        newPosition.y = PlayerTransform.position.y + 2;
         transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
 
         //Camera Bounds
