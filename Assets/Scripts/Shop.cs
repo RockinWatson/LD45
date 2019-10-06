f0bc09242d52cc7b90c1f4d165055377bb5439e9
Joshua Watson <josh.m.watson@gmail.com>
2019-10-06 13:54:53 -0500

    Finished putting together Art in Shop Overlay

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scripts.Objects;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
@@ -10,18 +11,49 @@ namespace Assets.Scripts
         private PlayerData _player;
         private bool _shopLoaded;
 
-        public GameObject[] CostumeList;
+        public GameObject[] ShopItemList;
+        private int _shopIndex = 0;
 
         private void Update()
         {
             if (Player.PlayerInstance.IsShopLoaded)
             {
                 Pause();
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    Debug.Log("MoveRight!!");
+                    if (_shopIndex < ShopItemList.Length)
+                    {
+                        _shopIndex++;
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    if (_shopIndex > 0)
+                    {
+                        _shopIndex--;
+                        if (_shopIndex == 0)
+                        {
+                            _shopIndex = 0;
+                        }
+                    }
+                }
+                Button btn = GetChildGameObject(ShopItemList[_shopIndex], "Button");
+                btn.Select();
+                btn.OnSelect(null);
             }
             if (Input.GetKeyDown(KeyCode.Escape) && Player.PlayerInstance.IsShopLoaded)
             {
                 Resume();               
             }
+        }
+
+        private Button GetChildGameObject(GameObject gObject, string name) {
+            Button[] ts = gObject.transform.GetComponentsInChildren<Button>();
+            foreach (Button t in ts) {
+                if (t.gameObject.name == name) return t;
+            }
+            return null;
         }
 
         public void Resume() {
