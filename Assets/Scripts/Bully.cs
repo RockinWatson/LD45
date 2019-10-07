8b77c8b634838c7d240d7d22c17741d05fdf3521
Joshua Watson <josh.m.watson@gmail.com>
2019-10-06 19:27:40 -0500

    Added Bully targeting

--- Assets/Scripts/Bully.cs
+++ Assets/Scripts/Bully.cs
@@ -4,6 +4,20 @@ namespace Assets.Scripts
 {
     public class Bully : MonoBehaviour
     {
+        public float speed;
 
+        public Transform target;
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+            if (Vector2.Distance(transform.position, target.position) < 4)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+        }
     }
 }
