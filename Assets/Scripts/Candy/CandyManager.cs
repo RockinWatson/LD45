e916fe6c70310cb4f4fe2ae6bfd521e4133849fa
Greg Raab <halfskye@gmail.com>
2019-10-06 17:11:44 -0700

    Candy Scripts

---
+++ Assets/Scripts/Candy/CandyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _candies = null;
+
+    static private CandyManager _instance = null;
+    static public CandyManager Get() { return _instance; }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public Candy GetCandy(Vector3 pos)
+    {
+        //@TODO: Instantiate candy, set position, set color?
+        GameObject go = Instantiate(GetRandomCandyPrefab(), pos, Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.up));
+        return go.GetComponent<Candy>();
+    }
+
+    private GameObject GetRandomCandyPrefab()
+    {
+        return _candies[Random.Range(0, _candies.Count)];
+    }
+}
