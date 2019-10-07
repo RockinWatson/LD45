4f3ae34626542d7d90090c8dca87ded6a72b5360
Greg Raab <halfskye@gmail.com>
2019-10-06 22:53:06 -0700

    Add CostumeManager for costume swaps

---
+++ Assets/Scripts/CostumeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Assets.Scripts;
+
+public class CostumeManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<RuntimeAnimatorController> _mainAnimatorControllers = null;
+
+    public void SetCostume(int index)
+    {
+        Player player = Player.PlayerInstance;
+
+        Animator animator = player.GetComponent<Animator>();
+        animator.runtimeAnimatorController = _mainAnimatorControllers[index];
+    }
+
+    private void Update()
+    {
+        //@TEMP:
+        DebugInput();
+    }
+
+    private void DebugInput()
+    {
+        int numCostumes = _mainAnimatorControllers.Count;
+        for (int i = 0; i < numCostumes; ++i)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                SetCostume(i);
+            }
+        }
+    }
+}
